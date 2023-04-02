caebbb7667a3ef4d1b1a6155d92b4c5b68685a3c
thecodekitchen <andy@thecodekitchen.xyz>
2023-04-02 19:30:23 -0400

    test edit

--- bot.ts
+++ bot.ts
@@ -4,35 +4,33 @@ deploy.init({
     env: true
   })
   
-  const commands = await deploy.commands.all()
-  if (commands.size !== 1) {
-    deploy.commands.bulkEdit([
-      {
-        name: 'ping',
-        description: "It's literally ping command. What did you expect?",
-        options: [
-          {
-            name: 'pingarg',
-            description: 'Again literally pingArg',
+deploy.commands.bulkEdit([
+    {
+    name: 'ping',
+    description: "It's literally ping command. What did you expect?",
+    options: [
+        {
+        name: 'pingarg',
+        description: 'Again literally pingArg',
+        required: false,
+        type: deploy.ApplicationCommandOptionType.STRING
+        }
+    ]
+    },
+    {
+    name: 'test',
+    description: "Testing modularity",
+    options: [
+        {
+            name: 'stuff',
+            description: 'Stuff',
             required: false,
             type: deploy.ApplicationCommandOptionType.STRING
-          }
-        ]
-      },
-      {
-        name: 'test',
-        description: "Testing modularity",
-        options: [
-            {
-                name: 'stuff',
-                description: 'Stuff',
-                required: false,
-                type: deploy.ApplicationCommandOptionType.STRING
-            }
-        ]
-      }
-    ])
-  }
+        }
+    ]
+    }
+])
+  
   
   deploy.handle('ping', (d) => {
     const arg = d.option<string | undefined>('pingarg')
@@ -40,6 +38,6 @@ deploy.init({
   })
 
   deploy.handle('test', (d) => {
-    const stuff = d.option<string| undefined>('stuff')
-    d.reply(`You said ${stuff !== undefined? stuff: 'nothing'}`)
+    const stuff = d.option<string | undefined>('pingarg')
+    d.reply(`Pong! You typed: ${stuff !== undefined ? stuff : 'nothing'}`)
   })
