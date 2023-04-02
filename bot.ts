ba8cafe8fd9fb6b9bc5fdf50dd58b609d93faeac
thecodekitchen <andy@thecodekitchen.xyz>
2023-04-02 19:01:26 -0400

    initial commit

---
+++ bot.ts
@@ -0,0 +1,29 @@
+import { deploy, config } from './deps.ts';
+
+deploy.init({
+    publicKey: config().PUBLIC_KEY,
+    token: config().TOKEN
+  })
+  
+  const commands = await deploy.commands.all()
+  if (commands.size !== 1) {
+    deploy.commands.bulkEdit([
+      {
+        name: 'ping',
+        description: "It's literally ping command. What did you expect?",
+        options: [
+          {
+            name: 'pingarg',
+            description: 'Again literally pingArg',
+            required: false,
+            type: deploy.ApplicationCommandOptionType.STRING
+          }
+        ]
+      }
+    ])
+  }
+  
+  deploy.handle('ping', (d) => {
+    const arg = d.option<string | undefined>('pingarg')
+    d.reply(`Pong! You typed: ${arg !== undefined ? arg : 'nothing'}`)
+  })
---
+++ deps.ts
@@ -0,0 +1,2 @@
+export * as deploy from "https://deno.land/x/harmony@v2.8.0/deploy.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
