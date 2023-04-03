2010f6cb83e4544baa6f71409dfd2a9006217819
thecodekitchen <andy@thecodekitchen.xyz>
2023-04-03 14:03:45 -0400

    delete old cmds

--- bot.ts
+++ bot.ts
@@ -7,6 +7,9 @@ deploy.init({
   })
 const guildId = '1054498243100295170'
 
+deploy.commands.delete("1092146274578473061")
+deploy.commands.delete("1092229890549497946")
+deploy.commands.delete("1092231614219698186")
 // deploy.commands.bulkEdit([
 //     add_beast
 // ], guildId)
